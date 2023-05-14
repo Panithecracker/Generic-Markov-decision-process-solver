6978bfe2b5f1b5781c82de989027bf32c9d96e6b
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-14 14:54:17 +0200

    Update mdp.py
    little change to trantion printing order

--- mdp.py
+++ mdp.py
@@ -3,9 +3,12 @@
 from typing import Dict
 # this is just to refer to the data type Dictionary for readability in the following lines of code
 
+import re # for parsing the file
+
 
 class State:
-    def __init__(self, val,g):  # a state object is characterized by a numerical value and boolean attribute goal
+
+    def __init__(self, val,g = None):  # a state object is characterized by a numerical value and boolean attribute goal
         self.state_val = val
         self.goal = g
 
@@ -53,36 +56,16 @@ class Transition:
         return hash((self.state_from, self.state_to, self.through))
 
     def __str__(self):
-        return f"Transition({self.state_from},{self.through} .{self.state_to})"
-
-
-class Cost:
-    def __init__(self, s: State, a: Action, C):
-        # a cost is characterized by one state object , an action object  and a numerical value defining its cost
-        self.through = a
-        self.state_from = s
-        self.cost = C
-
-    def __eq__(self, other):
-        if isinstance(other, Cost):
-            return self.through == other.through and self.state_from == other.state_from and self.cost == other.cost
-        return False
-
-    def __hash__(self):
-        return hash((self.through, self.state_from, self.cost))
-
-    def __str__(self):
-        return f"Cost({self.state_from}, {self.through}, {self.cost})"
+        return f"Transition({self.state_from},{self.state_to},{self.through})"
 
 
 class MDP:
 
-    def __init__(self, S, T, A, C, IC):
+    def __init__(self, S = None, T = None, A = None, IC = None):
         # characterized by a set of States, transitions, actions and costs objects
         self.States = S  # set of state objects
         self.Transitions = T  # dictionary: key is a transition and value a probability (Transition model)
         self.Actions = A  # set of action objects
-        self.Costs = C  # set of cost objects
         self.ImmediateCosts = IC  # dictionary where each key is an action object and values its associated cost
 
     def PrintAll(self):
@@ -141,34 +124,3 @@ class MDP:
                         opt_action = a # update the optimal action found for the state s
                 OP[s] = opt_action
         return OP  # in the end, the dictionary OP contains the optimal actions for all the sates in the MDP object
-
-
-
-
-s1 = State(1,0)
-s2 = State(2,0)
-s3 = State(3,1)
-S = {s2, s1, s3} #set of states
-a = Action("right")
-A = {a}
-t1 = Transition(s1,s1,a)
-t2 = Transition(s1,s2,a)
-t3 = Transition(s1,s3,a)
-t4 = Transition(s2,s1,a)
-t5 = Transition(s2,s2,a)
-t6 = Transition(s2,s3,a)
-t7 = Transition(s3,s1,a)
-t8 = Transition(s3,s2,a)
-t9 = Transition(s3,s3,a)
-IC = {a:1}
-T = {t1:0.2, t2:0.8, t3:0, t4:0, t5:0.2, t6:0.8, t7:0.8, t8:0.0, t9:0.2}
-
-my_mdp = MDP(S, T, A, None, IC)
-my_mdp.PrintAll()
-Values = my_mdp.ValueIteration()
-Opt = my_mdp.OptimalPolicy()
-for i in Values:
-    print(str(i) + " = " + str(Values[i]))
-for j in Opt:
-    print(str(j) + " = " + str(Opt[j]))
-
