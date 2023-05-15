2dd8dbbebf1df7b7a50353dddcae68a705e378d7
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-15 23:56:37 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,6 +7,8 @@ def parse_file(file):
     states = set()
     actions = set()
     transitions = {}
+    goal_S = 0
+    goal_T = 0
 
     # Open the input file
     with open(file, 'r') as f:
@@ -14,38 +16,42 @@ def parse_file(file):
         for line in f:
             # Match the line that starts with "states:"
             if line.lower().startswith('states:'):
-                print()
+                # print()
                 # Use regular expressions to find all states in the line
                 matches = re.findall(r'([\w.]+(?:)?\*?)', line)[1:]
                 for match in matches:
                     # Create a State object for each state found
                     val = match.rstrip('*')
                     g = 1 if match.endswith('*') else 0
+                    if g == 1:
+                        goal_S = 1
                     state = State(val, g)
-                    print(state)
+                    # print(state)
                     states.add(state)
-                print("S = " + str(states))
+                # print("S = " + str(states))
+                if goal_S != 1:
+                    print("\nGoal state was not introduced in <States line>.\nPlease use the flag * after inserting a goal state.\ne.g.: States: s1, s2, s3*, s4")
             # Match the line that starts with "actions:"
             elif line.lower().startswith('actions:'):
-                print()
+                # print()
                 # Extract the list of actions
                 action_names = re.findall(r'\b[\w.]+\b', line)[1:]
                 for name in action_names:
                     action = Action(name)
-                    print(action)
+                    # print(action)
                     actions.add(action)
-                print("A = " + str(actions))
+                # print("A = " + str(actions))
             # Match the line that starts with "costs:"
             elif line.lower().startswith('costs:'):
-                print()
+                # print()
                 # Extract the costs
                 costs = re.findall(r'c\(\s*([\w.]+)\)\s*=\s*(\d+(?:\.\d+)?)', line)
                 # Convert the costs to a dictionary
                 costs = {Action(action): float(cost) for action, cost in costs}
-                print("IC = " + str(costs))
+                # print("IC = " + str(costs))
             # Match the lines that start with "transitions:"
             elif line.lower().startswith('transitions:'):
-                print()
+                # print()
                 matches = re.findall(r'T\(\s*([\w.]+(?:)?\*?),\s*([\w.]+(?:)?\*?),\s*([\w.]+)\)\s*=\s(\d+(?:\.\d+)?)', line)
                 for match in matches:
                     p_val = match[0].rstrip('*')
@@ -56,17 +62,23 @@ def parse_file(file):
                     q_goal = 1 if match[1].endswith('*') else 0
                     q = State(q_val, q_goal)
 
+                    if p_goal == 1 or q_goal == 1:
+                        goal_T = 1
+
                     a_name = match[2]
                     a = Action(a_name)
 
                     prob = float(match[3])
                     transition = Transition(p, q, a)
-                    print(transition)
+                    # print(transition)
 
                     transitions[transition] = prob
-                print("T = " + str(transitions))
+                # print("T = " + str(transitions))
+                if goal_T != 1:
+                    print("\nGoal state was not introduced in <Transition line>.\nPlease use the flag * after inserting a goal state.\ne.g.: Transitions: T(s1,s1,act1) = 0.3, T(s1,s2*,act2) = 0.9")
+                    print("\nAlso, check that the goal states introduced in the transitions line are the same as the ones in the states line!")
 
-    return states, actions, costs, transitions
-
-# states, actions, costs, transitions = parse_file('pruebas.txt')
-
+    if goal_S != 0 and goal_T != 0:
+        return states, actions, costs, transitions
+    else:
+        return None, None, None, None
