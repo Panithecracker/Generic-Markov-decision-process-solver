41e42f3090e530fc41561eff915f3e9bc42a924f
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-05-15 23:56:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from setUp import setUp_mdpSolver
-from mdp import State, Transition, Action, MDP
+from setUp import setUp_mdpSolver, askFile
 
-file = input("Introduce name of file that contains your MDP problem: ")
-basic_mdp = setUp_mdpSolver(file)
-policy = basic_mdp.OptimalPolicy()
+file = askFile()
+user_mdp = setUp_mdpSolver(file)
+if user_mdp.States and user_mdp.Actions and user_mdp.Transitions and user_mdp.ImmediateCosts:
+    V = user_mdp.ValueIteration()
+    policy = user_mdp.OptimalPolicy(V)
+    user_mdp.WriteOptimalPolicy(policy)
 
-#The user can provide the model data through a well formatted text file
+    # Print the Value function for each state
+    print("\nValue function for each state:\n")
+    for state in V:
+        print(f"V( {state} ) = {V[state]}")
 
-print("OPTIMAL POLICY :")
-for state in policy:
-   print(f"{state} -> Optimal: {policy[state]}")
+    # Print the resulting policy
+    print("\n\nOptimal Policy:\n")
+    for state in policy:
+        print(f"{state} -> Optimal: {policy[state]}")
 
-print('Contents have been successfully written in a file called "OptimalPolicy.txt" ')
-basic_mdp.WriteOptimalPolicy(policy)
+else:
+    print("\nIntroduce your mdp definition with the expected file format, please!")
